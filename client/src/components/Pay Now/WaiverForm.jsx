c725fba2bdfcf69772da2b46d8ffb7cdbbc53377
adeenasyed <adeenasyed@icloud.com>
2026-07-12 00:39:41 -0400

    update

--- client/src/components/Pay Now/WaiverForm.jsx
+++ client/src/components/Pay Now/WaiverForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = '/resources/waiver/pdf.worker.js';
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import { Document, Page } from 'react-pdf';
